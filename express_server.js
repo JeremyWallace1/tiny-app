2fe9d5cda996e6694c4685e9bbefed2074638f63
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-30 21:34:04 -0700

    installed cookie-session middleware

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cookieParser = require('cookie-parser');
 const crypto = require("crypto");
-//const { clear } = require("console");
+const { clear } = require("console");
 const morgan = require('morgan');
 const bcrypt = require("bcryptjs");
 
